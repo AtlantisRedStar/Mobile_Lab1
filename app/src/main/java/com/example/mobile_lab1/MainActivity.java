72f094eb36e3b33af1665556dc3164281fc27996
AtlantisRedStar <atlantisredstar@gmail.com>
2023-10-01 19:57:52 +0300

    Added second button and change color functionality

--- app/src/main/java/com/example/mobile_lab1/MainActivity.java
+++ app/src/main/java/com/example/mobile_lab1/MainActivity.java
@@ -1,11 +1,10 @@
 package com.example.mobile_lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,9 +16,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         this.tvMain = findViewById(R.id.tvMain);
+
+
+        Button btnChangeColor = findViewById(R.id.btnChangeColor);
     }
 
     public void onBtnChangeTextClick(View view) {
         this.tvMain.setText("Changed Text");
+    }
+
+    public void onBtnChangeColorClick(View view) {
+
+        this.tvMain.setTextColor(0xFFFF0000);
     }
 }
